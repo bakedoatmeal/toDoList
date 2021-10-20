53d13e2fac99c28d18b353c8a6507db0912a29be
Jane Cui <jane.cui@shopify.com>
2021-10-20 14:14:39 -0400

    added functionality to update route

--- app.py
+++ app.py
@@ -48,7 +48,13 @@ def update(id):
     task = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
-        pass
+        task.content = request.form['content']
+
+        try: 
+            db.session.commit()
+            return redirect('/')
+        except: 
+            return 'There was a problem updating your task'
     else: 
         return render_template('update.html', task=task)
 
